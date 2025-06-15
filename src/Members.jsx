245325bf7f8e45d11d6d89a4fdb9dfbcb4d391f9
Paul Fraenkel <pfraenkel@live.fr>
2025-06-15 18:20:27 +0200

    Members

--- src/Members.jsx
+++ src/Members.jsx
@@ -2,21 +2,67 @@ import Navbar from "./Navbar.jsx";
 
 const members_data = [
   {
-    name:"Mikhail Kataevskii",
-    photo:"./src/assets/24kataevsk.jpg"
+    name:"Mikhail KATAEVSKII",
+    photo:"./src/assets/24kataevsk.jpg",
+    status:"President",
+    phone:"+33 7 12 34 56 78",
+    mail:"mikhail.kataevskii@etu.minesparis.psl.eu"
   },
   {
-    name:"Lucas Duhautois",
-    photo:"./src/assets/24duhautoi.jpg"
-  }
+    name:"Lucas DUHAUTOIS",
+    photo:"./src/assets/24duhautoi.jpg",
+    status:"Treasurer",
+    mail:"lucas.duhautois@etu.minesparis.psl.eu"
+  },
+  {
+    name:"Laurentin-Wilhelm AHREND",
+    photo:"./src/assets/24ahrend.jpg",
+    status:"Vice-president",
+    phone:"+33 7 13 57 92 46",
+    mail:"laurentin-wilhelm.ahrend@etu.minesparis.psl.eu"
+  },
+  {
+    name:"Pierre-Louis COPPENS",
+    photo:"./src/assets/24coppens.jpg",
+    status:"Head of events",
+    phone:"+33 6 11 22 33 44",
+    mail:"pierre-louis.coppens@etu.minesparis.psl.eu"
+  },
+  {
+    name:"Paul FRAENKEL",
+    photo:"./src/assets/24fraenkel.jpg",
+    status:"Head of partners",
+    phone:"+33 6 44 33 22 11",
+    mail:"paul.fraenkel@etu.minesparis.psl.eu"
+  },
 ];
 
 function Member(data) {
-  console.log("\"" + data.data.photo + "\"");
   return (
-    <div class="w-10/12 h-fit p-3 bg-gray-50">
-      <p class="text-black">{data.data.name}</p>
-      <img src={data.data.photo}/>
+    <div class="px-4 my-5 flex justify-center">
+      <div class="h-fit w-full max-w-2xl p-8 bg-white inline-flex overflow-x-scroll">
+        <img src={data.data.photo} class="w-24 h-fit"/>
+        <div class="w-full pl-5">
+          <div class="w-fit border-b-1 border-gray-400 pb-2 mb-2">
+            <p class="text-2xl">{data.data.name}</p>
+            <p class="text-md">{data.data.status}</p>
+          </div>
+          <ul class="flex flex-col text-nowrap">
+            <Show when={data.data.phone} >
+              <li class="inline-flex items-center">
+                <i class="fa-solid fa-phone px-2"></i>
+                <p>{data.data.phone}</p> 
+              </li>
+            </Show>
+            <Show when={data.data.mail} >
+              <li class="inline-flex items-center">
+                <i class="fa-solid fa-envelope px-2"></i>
+                <a href={"mailto:" + data.data.mail}>{data.data.mail}</a> 
+              </li>
+            </Show>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
@@ -24,10 +70,12 @@ function Member(data) {
 function Members() {
   return (
     <>
-      <Navbar />
-      <For each={members_data}>
-        {(elem, i) => <Member data={elem} />}
-      </For>
+      <div class="bg-[url(/src/assets/bg-park.jpg)] bg-cover bg-fixed w-full h-screen overflow-scroll">
+        <Navbar />
+        <For each={members_data}>
+          {(elem, i) => <Member data={elem} />}
+        </For>
+      </div>
     </>
   );
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,15 @@
 import { A } from "@solidjs/router";
 
+//<A activeClass="text-blue-900" end={_end} href={_href}>{_name}</A>
+
 function Navbar() {
   return (
-    <div>
-      <A activeClass="text-blue-900" end={true} href="/">Home</A>
-      <A activeClass="text-blue-900" href="/about">About</A>
-      <A activeClass="text-blue-900" href="/events">Events</A>
-      <A activeClass="text-blue-900" href="/members">Members</A>
-      <A activeClass="text-blue-900" href="/contact">Contact</A>
+    <div class="flex justify-around bg-white overflow-x-scroll sticky top-0 shadow-md">
+      <A class="text-center p-2 w-full" inactiveClass="bg-white hover:bg-gray-200" activeClass="bg-blue-200" end={true} href="/">Home</A>
+      <A class="text-center p-2 w-full" inactiveClass="bg-white hover:bg-gray-200" activeClass="bg-blue-200" href="/about">About</A>
+      <A class="text-center p-2 w-full" inactiveClass="bg-white hover:bg-gray-200" activeClass="bg-blue-200" href="/events">Events</A>
+      <A class="text-center p-2 w-full" inactiveClass="bg-white hover:bg-gray-200" activeClass="bg-blue-200" href="/members">Members</A>
+      <A class="text-center p-2 w-full" inactiveClass="bg-white hover:bg-gray-200" activeClass="bg-blue-200" href="/contact">Contact</A>
     </div>
   );
 }
