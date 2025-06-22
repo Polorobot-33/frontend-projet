bf440e878233853a4ba4a15f6538185380e51e83
Pierre-Louis COPPENS <pierre-louis.coppens@etu.minesparis.psl.eu>
2025-06-22 22:41:09 +0200

    doc changes

--- src/Members.jsx
+++ src/Members.jsx
@@ -1,47 +1,48 @@
 import Navbar from "./Navbar.jsx";
 import { writeClipboard } from "@solid-primitives/clipboard";
-import members_data from "./assets/members_data.json";
+import members_data from "./assets/members_data.json"; //les membres et données sont stockés dans un fichier json
 
 //Solid Element for displaying the informations of one member
-//data : Member information, given in the format of members_data.json : 
+//data : Member information, given in the format of members_data.json :
 //  name : String
 //  status : String
 //  photo : String, relative uri of the picture
 //  phone : String
 //  mail : String
 
+{
+  /* la fonction Member(data) donne le code HTML d'affichage d'un seul membre, elle prend en compte la présence ou l'absence de certajnes données */
+}
 function Member(data) {
   return (
-    <div class="m-5 min-w-fit w-2xl inline-flex justify-center snap-start">
+    <div class="m-5 inline-flex w-2xl min-w-fit snap-start justify-center">
       <div class="mt-4 flex overflow-hidden rounded-2xl p-4 text-center text-sm text-white [box-shadow:inset_1px_1px_1px_rgba(255,255,255,0.75)] brightness-110 saturate-100 backdrop-blur-sm dark:[box-shadow:inset_1px_1px_1px_rgba(255,255,255,0.15)]">
-        {/* Picture */}
         <img src={data.data.photo} class="h-fit w-24 rounded-xl" />
         <div class="w-full pl-5">
-
-          {/* Name and Position */}
           <div class="mb-2 w-fit border-b-1 border-gray-400 pb-2">
             <p class="text-2xl">{data.data.name}</p>
             <p class="text-base">{data.data.status}</p>
           </div>
           <ul class="flex flex-col text-nowrap">
-            {/* Display the phone number only when provided in data */}
             <Show when={data.data.phone}>
               <li class="inline-flex items-center text-black duration-300 hover:text-lg hover:text-purple-800">
                 <i class="fa-solid fa-phone px-2"></i>
 
-                {/* copy the phone number to the clip board when clicked and alert the user */}
-                <button onClick={() => {writeClipboard(data.data.phone); alert("Phone number copied")}}>
+                <button
+                  onClick={() => {
+                    writeClipboard(data.data.phone);
+                    alert("Phone number copied");
+                  }}
+                >
                   {data.data.phone}
                 </button>
               </li>
             </Show>
-            
-            {/* Display the mail adress only when provided in data */}
+
             <Show when={data.data.mail}>
               <li class="inline-flex items-center text-black duration-300 hover:text-lg hover:text-purple-800">
                 <i class="fa-solid fa-envelope px-2"></i>
 
-                {/* Open the default mail editor when clicked (This feature may not work) */}
                 <a href={`mailto:${data.data.mail}`}>{data.data.mail}</a>
               </li>
             </Show>
@@ -52,6 +53,9 @@ function Member(data) {
   );
 }
 
+{
+  /* la fonction Members() fait appel à Member(data) pour afficher tous les membres listés*/
+}
 function Members() {
   return (
     <>
@@ -60,13 +64,11 @@ function Members() {
           Voici nos membres les plus illustres
         </h1>
 
-        {/* Display all the members using a for loop */}
-        <div class="flex h-3/4 w-full flex-col justify-around items-center overflow-scroll snap-y">
+        <div class="flex h-3/4 w-full snap-y flex-col items-center justify-around overflow-scroll">
           <For each={members_data}>{(elem, i) => <Member data={elem} />}</For>
         </div>
       </div>
 
-      {/* Menu de navigation entre les pages */}
       <Navbar />
     </>
   );
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,8 @@
 import { A } from "@solidjs/router";
 
-/* Style CSS du bouton de la page active */
 const activeStyle =
   "text-white font-medium text-base px-4 py-2 rounded-lg transition-colors duration-200 bg-white/20";
 
-/* Style CSS du bouton menant aux autres pages */
 const inactiveStyle =
   "text-white font-medium text-base px-4 py-2 rounded-lg transition-colors duration-200 hover:bg-white/10";
 
@@ -13,14 +11,13 @@ export default function Navbar() {
     <>
       <div class="absolute bottom-8 left-0 z-20 w-full">
         <nav class="relative mx-auto w-full max-w-lg overflow-hidden rounded-2xl bg-transparent">
-          {/* Glassmorphism */}
           <div class="absolute inset-0 rounded-2xl brightness-110 saturate-150 [filter:url(#glass-distortion)] backdrop-blur-sm"></div>
 
           <div class="absolute inset-0 rounded-2xl [box-shadow:inset_1px_1px_1px_rgba(255,255,255,0.75)] dark:[box-shadow:inset_1px_1px_1px_rgba(255,255,255,0.15)]"></div>
 
           <div class="relative z-10 p-4">
             <ul class="flex justify-around">
-              {/* Bouton vers la page d'accueil */}
+              {/* on définit les boutons de la navbar successivement*/}
               <li>
                 <A
                   href="/"
@@ -31,7 +28,6 @@ export default function Navbar() {
                   Accueil
                 </A>
               </li>
-              {/* Bouton vers la page de renseignements sur l'association */}
               <li>
                 <A
                   href="/About"
@@ -41,7 +37,6 @@ export default function Navbar() {
                   À propos
                 </A>
               </li>
-              {/* Bouton vers la page qui présente les membres de l'association */}
               <li>
                 <A
                   href="/Members"
@@ -51,7 +46,6 @@ export default function Navbar() {
                   Membres
                 </A>
               </li>
-              {/* Bouton vers la page qui permet d'envoyer un message à l'association */}
               <li>
                 <A
                   href="/Contact"
