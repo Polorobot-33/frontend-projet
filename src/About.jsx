691bd930bfe26d3a5addf978fa1c7f84b37d90d0
Laurentin <laurentin-wilhelm.ahrend@etu.minesparis.psl.eu>
2025-06-22 16:51:28 +0200

    correction

--- src/About.jsx
+++ src/About.jsx
@@ -15,13 +15,11 @@ function About() {
             <h1 class="text-5xl font-bold text-white">Mines de Sel</h1>
           </div>
           <p class="relative mx-auto mt-4 w-[90vw] max-w-[400px] overflow-hidden rounded-2xl p-4 text-center text-sm text-white [box-shadow:inset_1px_1px_1px_rgba(255,255,255,0.75)] brightness-110 saturate-150 backdrop-blur-sm dark:[box-shadow:inset_1px_1px_1px_rgba(255,255,255,0.15)]">
-            Notre mission est de répandre le plus de sel possible à travers le
-            monde entier. Leurs raisons sont nombreuses et jamais légitime, mais
-            vous avez face à vous les mineurs les plus aigris de la promo. Ils
-            ont même essayé de reproduire le liquidglass de apple dans un but
-            satirique mais le résultat est un site très lent à charger aux
-            visuels médiocres qui est aussi responsive que Basile Marchand quand
-            il n'a pas bu de café.
+           Notre mission est de répandre le plus de sel possible à travers le monde entier. 
+           Nos raisons sont nombreuses et jamais légitimes, mais vous avez en face de vous 
+           les mineurs les plus aigris de la promo. Nous avons même essayé de reproduire le 
+           liquid glass d’Apple dans un but satirique, mais le résultat est un site très lent à charger, 
+           aux visuels médiocres, et aussi responsive que Basile Marchand quand il n’a pas bu de café.
             <br />
             <br />
             P.S. Oui 3 milliard d'arbres sont brulés à chaque fois que ce fond
