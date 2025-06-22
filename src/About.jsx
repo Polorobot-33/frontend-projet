b9b0819eb41af643a916cd4b1991a6bd69d0b001
Laurentin <laurentin-wilhelm.ahrend@etu.minesparis.psl.eu>
2025-06-22 14:14:51 +0200

    seum

--- src/About.jsx
+++ src/About.jsx
@@ -16,7 +16,7 @@ function About() {
         </div>
         <p class="text-lg text-white text-center p-6">
           Notre mission est de répande le plus de sel possible à travers le
-          monde entier. Leurs raisons sont nombreuses et jamais légitime, mais vous avez face à vous les mineurs les plus aigris de la promo. Il ont même essayé de reproduire le liquidglass de apple dans un but satirique mais le résultat est un site très lent a charger aux visuels médiocres qui est aussi responsive que Basile Marchand quand il n'a pas bu son café.
+          monde entier. Leurs raisons sont nombreuses et jamais légitime, mais vous avez face à vous les mineurs les plus aigris de la promo. Ils ont même essayé de reproduire le liquidglass de apple dans un but satirique mais le résultat est un site très lent à charger aux visuels médiocres qui est aussi responsive que Basile Marchand quand il n'a pas bu de café.
           <br/>
           P.S. Oui 3 milliard d'arbres sont brulés à chaque fois que ce fond
           d'écran de 98Mo est chargé
